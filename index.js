ac973ba015dfb87ac850b71cfc9d00fbffa6fea0
first-name second-name <silvia.wachira@student.moringa.com>
2022-08-04 12:08:59 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,8 @@
+function introductionWithLanguageOptional(name, language = "JaveScript") {
+    return "Hi, my name is ${name} and I am learning to program in ${language}."
+}
+
+
+// function sayHelloTo(firstName = "User") {
+//     console.log(`Hello, ${firstName}!`);
+// }
